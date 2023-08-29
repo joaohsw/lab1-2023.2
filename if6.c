85164965e960bf2cf7c62d8a1d917d9527767b01
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2023-08-29 02:00:58 -0300

    Add files via upload

--- if6.c
+++ if6.c
@@ -2,21 +2,21 @@
 #include<math.h>
 
 int main(){
+
     float altura;
-    int sexo;
+    char sexo;
+    
 
     printf("informe respectivamente sua altura e seu sexo (masculino ou feminino): ");
     scanf("%f %c", &altura, &sexo);
-    if (sexo == 1)
-        printf("Peso ideal: %.2f", (72.7 * altura) - 58);
+
+    if(sexo == 'm')
+        printf("Seu peso ideal eh: %.2f", (72.7 * altura) - 58);
     else
-        if (sexo == 2)
-        printf("Pesoa ideal: %.2f", (62.1 * altura) - 44.7);
-
-return 0;
+        if(sexo == 'f')
+            printf("Seu peso ideal eh: %.2f", (62.1 * altura) - 44.7);
     
-
-
+return 0;
     
 
 }
