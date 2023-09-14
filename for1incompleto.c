fef82804e65afcfaf28665ae4f48a83cb531588e
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2023-09-14 11:59:42 -0300

    Add files via upload

--- for1incompleto.c
+++ for1incompleto.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int main(){
-
-    int inicio, fim, cont;
+int main() {
+    int inicio, fim, cont, soma = 0;
+    
     printf("Informe os valores do inicio e do final de um intervalo: ");
     scanf("%d %d", &inicio, &fim);
-    for (cont = inicio; cont <= fim; cont++){
-        if (cont++ % 2 != 0){
-            printf("%d ", cont - 1);
-        }    
-    }   
-    for (cont++ % 2 != 0; cont <= fim; cont + cont) 
-        printf ("\n%d", cont);
-}
-        
     
+    for (cont = inicio; cont <= fim; cont++) {
+        if (cont % 2 != 0) {
+            printf("%d ", cont);
+            soma += cont;
+        }
+    }
+
+    printf("\nSoma dos numeros impares: %d\n", soma);
+
+    return 0;
+}
    
 
