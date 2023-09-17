de9950ba91e0e2a67f4b26e5a3e225ace6218346
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2023-09-17 20:43:36 -0300

    Update JoaoHenrique_cartas.c

--- JoaoHenrique_cartas.c
+++ JoaoHenrique_cartas.c
@@ -1,85 +1,73 @@
 #include <stdio.h>
 #include <time.h>
 
-int estrategia1(int cartaescolhida, int cartamudada) {
 
-    int i;
-    int resultado1;
+int estrategia1(int cartadefinida = 1 + rand() % 3;){ // as de espadas = 1 as de bastos = 2 sete de espadas = 3
 
-    for  (i = 1; i <= 3; i++){
-        cartaescolhida = rand(i) % 3; // as de espadas = 1, as de bastos = 2, sete de espadas = 3
-        for  (i = 1; i <= 3 && i != cartaescolhida; i++){
-            cartamudada = rand(i) % 3;
-            if(cartaescolhida == 1){
-                printf("\nL");
-                resultado1 == 5; // 5 representa que perdeu o jogo e 10 que ganhou
-                return 0;
-            }
-                else if (cartamudada == 1)
-                    printf("\nW");
-                    resultado1 == 10;
-                
-        }
-    }
-    return resultado1;
-
-}
-
-int estrategia2(int cartadefinida){
-
-    int i;
-    int resultado2;
-
-    for (i = 1; i <= 3; i++){
-        cartadefinida = rand(i) % 3;
         if (cartadefinida == 1){
-            printf("\nW");
-            resultado2 == 10;
+            return 10;  // 5 representa que perdeu o jogo e 10 que ganhou
         }
-            else 
-                printf("\nL");
-                resultado2 == 5;
+            else{ 
+                return 5;
+            }
             
     }
 
-    return resultado2;
+int estrategia2(int cartaescolhida = 1 + rand() % 3, int cartamudada = 1 + rand() % 3) {
 
-}
+    int i;
+    
+    for  (i = 1; i <= 3; i++){
+        cartamudada = 1 + rand() % 3;
+        if (cartamudada != cartaescolhida){
+            break;
+        }    
+    }   
+        if(cartaescolhida == 1){
+            return 5;
+            }
+            else {
+                return 10;         
+        }
+    }
+
 
 int main() {
 
     srand(time(NULL));
-    char w, l;
-    int i, simulacao, derrotas = 0, vitorias = 0, i2, derrotas2 = 0, vitorias2 = 0;
-    float porcentagemv, porcentagemd, porcentagemv2, porcentagemd2;
+    int simulacao = 1000000, derrotas1 = 0, vitorias1 = 0, i2, derrotas2 = 0, vitorias2 = 0, resultado1, resultado2;
+    float porcentagemv1, porcentagemd1, porcentagemv2, porcentagemd2;
     
 
     simulacao = 1000000;
 
-    for (i = 1; i <= simulacao; i++){
-        estrategia1;
-            if (i == 5){
-                derrotas++;
-            }
-            if (i == 10){
-                vitorias++;
-            }
-                porcentagemv == vitorias / 10000;
-                porcentagemd == derrotas / 10000;
+    for (int i = 1; i <= simulacao; i++){
+        resultado1 = estrategia1();
+        if (restultado1 == 5) {
+            derrotas1++;
+        }
+        if (resultado1 == 10) {
+            vitorias1++;
+        }
     }
 
     for (i2 = 1; i <= simulacao; i2++){
-        estrategia2;
-            if (i2 == 5){
-                derrotas2++;
-            }
-            if (i2 == 10){
-                vitorias2++;
-            }
+        resultado2 = estrategia2();
+        if (i2 == 5){
+            derrotas2++;
+        }
+        if (i2 == 10){
+            vitorias2++;
+        }
 
-                porcentagemv2 == vitorias2 / 10000;
-                porcentagemd2 == derrotas2 / 10000;
+                
     }
+
+    porcentagemv1 = (float)vitorias1 / simulacao * 100;
+    porcentagemd1 = (float)derrotas1 / simulacao * 100;
+    porcentagemv2 = (float)vitorias2 / simulacao * 100;
+    porcentagemd2 = (float)derrotas2 / simulacao * 100;
+    
 
     printf("> Estrategia 1 (sem mudanca de carta):\n  Partidas jogadas: 1000000");
     printf("\n  Partidas ganhas: %d", vitorias2);
@@ -93,8 +81,18 @@ int main() {
     printf("\n  Porcentagem de derrotas: %.2f%%", porcentagemd);
     printf("\n  Porcentagem de vitorias: %.2f%%\n", porcentagemv);
 
-    printf("\n> Recomendacao: ");
+    if (porcentagemv1 > porcentagemv2) {
+        printf("\n> Recomendacao: %d", porcentagemv1);
+    }
+    else if (porcentagemv2 > porcentagemv1) {
+        printf("\n> Recomendacao: %d", porcentagemv2);
+    }
+    else if {
+        return 0;
+    }
 
+    return 0;
+    
     }
                 
 
