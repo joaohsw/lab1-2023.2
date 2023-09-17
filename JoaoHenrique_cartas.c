5259b2595c5f16dbee3a635c2bfecff7f9451947
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2023-09-17 17:59:36 -0300

    Add files via upload

--- JoaoHenrique_cartas.c
+++ JoaoHenrique_cartas.c
@@ -6,9 +6,9 @@ int estrategia1(int cartaescolhida, int cartamudada) {
     int i;
     int resultado1;
 
-    for  (i == 1; i <= 3; i++){
+    for  (i = 1; i <= 3; i++){
         cartaescolhida = rand(i) % 3; // as de espadas = 1, as de bastos = 2, sete de espadas = 3
-        for  (i == 1; i <= 3 && i != cartaescolhida; i++){
+        for  (i = 1; i <= 3 && i != cartaescolhida; i++){
             cartamudada = rand(i) % 3;
             if(cartaescolhida == 1){
                 printf("\nL");
@@ -30,7 +30,7 @@ int estrategia2(int cartadefinida){
     int i;
     int resultado2;
 
-    for (i == 1; i <= 3; i++){
+    for (i = 1; i <= 3; i++){
         cartadefinida = rand(i) % 3;
         if (cartadefinida == 1){
             printf("\nW");
@@ -50,12 +50,13 @@ int main() {
 
     srand(time(NULL));
     char w, l;
-    int i, simulacao, derrotas = 0, vitorias = 0, porcentagemv, porcentagemd;
+    int i, simulacao, derrotas = 0, vitorias = 0, i2, derrotas2 = 0, vitorias2 = 0;
+    float porcentagemv, porcentagemd, porcentagemv2, porcentagemd2;
     
 
     simulacao = 1000000;
 
-    for (i == 1; i <= simulacao; i++){
+    for (i = 1; i <= simulacao; i++){
         estrategia1;
             if (i == 5){
                 derrotas++;
@@ -67,12 +68,40 @@ int main() {
                 porcentagemd == derrotas / 10000;
     }
 
-        
+    for (i2 = 1; i <= simulacao; i2++){
+        estrategia2;
+            if (i2 == 5){
+                derrotas2++;
+            }
+            if (i2 == 10){
+                vitorias2++;
+            }
+
+                porcentagemv2 == vitorias2 / 10000;
+                porcentagemd2 == derrotas2 / 10000;
+    }
+
+    printf("> Estrategia 1 (sem mudanca de carta):\n  Partidas jogadas: 1000000");
+    printf("\n  Partidas ganhas: %d", vitorias2);
+    printf("\n  Partidas perdidas: %d", derrotas2);
+    printf("\n  Porcentagem de derrotas: %.2f%%", porcentagemd2);
+    printf("\n  Porcentagem de vitorias: %.2f%%\n", porcentagemv2);
+
+    printf("\n> Estrategia 2 (com mudanca de carta):\n  Partidas jogadas: 1000000");
+    printf("\n  Partidas ganhas: %d", vitorias);
+    printf("\n  Partidas perdidas: %d", derrotas);
+    printf("\n  Porcentagem de derrotas: %.2f%%", porcentagemd);
+    printf("\n  Porcentagem de vitorias: %.2f%%\n", porcentagemv);
+
+    printf("\n> Recomendacao: ");
+
+    }
+                
 
 
 
 
-}
+
    
 
 
