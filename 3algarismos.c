34144bdf0c440e019bb767041ae2375a703dac0f
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2023-11-18 17:15:32 -0300

    delete

--- 3algarismos.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main(){
-   int inteiro;
-   printf("Digite um numero inteiro com 3 algarismos: ");
-   scanf("%d", &inteiro);
-   printf("Numero digitado: %d\nAlgarismo das dezenas: %d", inteiro, (inteiro % 100)/10);
-}
